d718e8ae17f985b0496c7fdaa4fb9f094b170cf0
Fredrik Svensson <fs222id@student.lnu.se>
2024-05-03 23:43:43 +0200

    Elasticsearch configuration updated for HTTPS connection and authentication

--- src/config/elasticsearch.js
+++ src/config/elasticsearch.js
@@ -1,10 +1,31 @@
 import { Client } from '@elastic/elasticsearch'
+import fs from 'fs'
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 let client
 
 export function getElasticsearchClient() {
+  console.log('Path:', path.resolve(__dirname, 'http_ca.crt'))
+  console.log('File exists:', fs.existsSync(path.resolve(__dirname, 'http_ca.crt')))
+  console.log('Password:', process.env.ELASTIC_PASSWORD)
   if (!client) {
-    client = new Client({ node: 'http://localhost:9200' })
+    console.log('Creating new Elasticsearch client')
+    client = new Client({ 
+      node: 'https://localhost:9200',
+      auth: {
+        username: 'elastic',
+        password: process.env.ELASTIC_PASSWORD
+      },
+      ssl: {
+        ca: fs.readFileSync(path.resolve(__dirname, 'http_ca.crt')),
+        rejectUnauthorized: true
+      }
+    })
+    console.log('Elasticsearch client created', client)
   }
   return client
 }
