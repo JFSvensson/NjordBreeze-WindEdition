22381dd042a2450496e5fa94e4276fb32caa6c30
Fredrik Svensson <fs222id@student.lnu.se>
2024-05-06 18:33:15 +0200

    Fix color scheme and hide numbers

--- client/public/js/updateMarkers.js
+++ client/public/js/updateMarkers.js
@@ -15,19 +15,11 @@ export function updateMarkers(map, stations, timeToBegin, hoursPassed, displayEl
             const lat = station.location.coordinates[1]
             const lon = station.location.coordinates[0]
             const windSpeed = parseFloat(station.windSpeed)
-            if (isNaN(windSpeed)) {
+            if (isNaN(windSpeed) || windSpeed === 0) {
                 return
             }
             const windDirection = station.windDirection
             const color = getColorForWindSpeed(windSpeed)  // Get color based on wind speed
-
-            const speedMarker = L.marker([lat, lon], {
-                icon: L.divIcon({
-                    className: 'wind-speed-marker',
-                    html: `<span>${windSpeed.toFixed(1)}</span>`,
-                    iconSize: [30, 30]
-                })
-            }).addTo(map)
 
             const directionMarker = L.marker([lat, lon], {
                 icon: L.divIcon({
@@ -38,7 +30,15 @@ export function updateMarkers(map, stations, timeToBegin, hoursPassed, displayEl
                 })
             }).addTo(map)
 
-            markers.push(speedMarker)
+            // const speedMarker = L.marker([lat, lon], {
+            //     icon: L.divIcon({
+            //         className: 'wind-speed-marker',
+            //         html: `<span>${windSpeed.toFixed(1)}</span>`,
+            //         iconSize: [30, 30]
+            //     })
+            // }).addTo(map)
+
+            // markers.push(speedMarker)
             markers.push(directionMarker)
         }
     })
@@ -47,14 +47,12 @@ export function updateMarkers(map, stations, timeToBegin, hoursPassed, displayEl
 }
 
 function getColorForWindSpeed(windSpeed) {
-    if (windSpeed < 5) {
-        return '#00bfff'; // Ljusblå för svaga vindar
-    } else if (windSpeed < 15) {
-        return '#007fff'; // Mörkblå för måttliga vindar
-    } else if (windSpeed < 25) {
-        return '#ffbf00'; // Gul för starka vindar
-    } else {
-        return '#ff0000'; // Röd för mycket starka vindar
-    }
-}
+    // Normalise the wind speed to a value between 0 and 1
+    const normalizedSpeed = Math.min(windSpeed / 33, 1) // Maximum is 33 m/s (Orkan)
 
+    let lightness = 70 - (normalizedSpeed * 70) // Start at 70%, going to 0%
+    let saturation = 100
+    let hue = 0
+
+    return `hsl(${hue}, ${saturation}%, ${lightness}%)`
+}
