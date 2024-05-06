e2ebac3a772a9dced67508afe827ca54de0ab124
Fredrik Svensson <fs222id@student.lnu.se>
2024-05-06 10:14:12 +0200

    Add change over time

--- client/public/js/app.js
+++ client/public/js/app.js
@@ -1,53 +1,72 @@
 document.addEventListener('DOMContentLoaded', function() {
     const map = L.map('map').setView([62.0, 15.0], 5) // Center of Sweden
+    let date = new Date()
+    date.setMonth(date.getMonth() - 1)
+    let timestampIndex = date.getTime()
+    let stations = []
   
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map)
   
-    fetch('https://svenssonom.se/njordbreeze-we/api/v1/smhi/all-wind-speed-data') // Adjust the URL to your API
+    fetch('https://svenssonom.se/njordbreeze-we/api/v1/smhi/all-wind-speed-data')
       .then(response => response.json())
       .then(data => {
-        const points = Object.values(data)
+        stations = Object.values(data)
           .filter(station => station.location && station.location.coordinates)
-          .map(station => {
+        updateMarkers()
+        setInterval(updateMarkers, 5000) // Update every 5 seconds
+      })
+
+      function updateMarkers() {
+        map.eachLayer(layer => {
+            if (layer instanceof L.Marker) {
+                map.removeLayer(layer)
+            }
+        })
+
+        stations.forEach(station => {
             const lat = station.location.coordinates[1]
             const lon = station.location.coordinates[0]
-            const intensity = parseFloat(station.windSpeed)
+            const windData = station.windData.find(item => item.date === timestampIndex)
+            if (!windData) {
+            return
+            }
+            const intensity = parseFloat(windData.windSpeed)
+            const direction = windData.windDirection
 
             // Check if intensity is NaN
             if (isNaN(intensity)) {
                 return null
             }
-  
-            // Add a marker for the station
-            L.marker([lat, lon], {
-            icon: L.divIcon({
-                className: 'wind-speed-marker',
-                html: intensity.toFixed(1), // Display the wind speed as the marker
-                iconSize: [30, 30] // Adjust the size of the marker
-            })
-            }).addTo(map)
 
-            // Add a marker for the station
+            // Add a marker for the wind speed
             L.marker([lat, lon], {
                 icon: L.divIcon({
-                className: 'wind-direction-arrow',
-                html: `<div style='transform: rotate(${station.windDirection}deg); transform-origin: center;'>➤</div>`,
-                iconSize: [30, 30], // Adjust the size of the marker
-                iconAnchor: [15, 15], // Center the icon on the coordinates
+                    className: 'wind-speed-marker',
+                    html: intensity.toFixed(1),
+                    iconSize: [30, 30]
                 })
-            }).addTo(map);
+            }).addTo(map)
 
-            return [lat, lon, intensity]
+            // Add a marker for the wind direction
+            L.marker([lat, lon], {
+                icon: L.divIcon({
+                    className: 'wind-direction-arrow',
+                    html: `<div style='transform: rotate(${direction}deg); transform-origin: center;'>➤</div>`,
+                    iconSize: [30, 30],
+                    iconAnchor: [15, 15]
+                })
+            }).addTo(map)
         }).filter(station => station !== null) // Filter out stations with a windSpeed of NaN
+
+        timestampIndex += 86400000 // Add one day
+    }
   
-        L.heatLayer(points, {
-            radius: 50,
-            blur: 30,
-            maxZoom: 15,
-            max: 33,
-        }).addTo(map)
-      })
-      .catch(error => console.error('Error loading the data:', error))
-  })
+    // L.heatLayer(points, {
+    //     radius: 50,
+    //     blur: 30,
+    //     maxZoom: 15,
+    //     max: 33,
+    // }).addTo(map)
+})
