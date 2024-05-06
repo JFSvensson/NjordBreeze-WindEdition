9da23d5bebe5060580f79887fb2c51d5e631b0ff
Fredrik Svensson <fs222id@student.lnu.se>
2024-05-06 08:43:35 +0200

    Add marker (arrow) for winddirection, filter out NaN

--- client/public/js/app.js
+++ client/public/js/app.js
@@ -14,18 +14,33 @@ document.addEventListener('DOMContentLoaded', function() {
             const lat = station.location.coordinates[1]
             const lon = station.location.coordinates[0]
             const intensity = parseFloat(station.windSpeed)
+
+            // Check if intensity is NaN
+            if (isNaN(intensity)) {
+                return null
+            }
   
-          // Add a marker for the station
-          L.marker([lat, lon], {
+            // Add a marker for the station
+            L.marker([lat, lon], {
             icon: L.divIcon({
-              className: 'wind-speed-marker',
-              html: intensity.toFixed(1), // Display the wind speed as the marker
-              iconSize: [30, 30] // Adjust the size of the marker
+                className: 'wind-speed-marker',
+                html: intensity.toFixed(1), // Display the wind speed as the marker
+                iconSize: [30, 30] // Adjust the size of the marker
             })
-          }).addTo(map)
-  
+            }).addTo(map)
+
+            // Add a marker for the station
+            L.marker([lat, lon], {
+                icon: L.divIcon({
+                className: 'wind-direction-arrow',
+                html: `<div style='transform: rotate(${station.windDirection}deg); transform-origin: center;'>➤</div>`,
+                iconSize: [30, 30], // Adjust the size of the marker
+                iconAnchor: [15, 15], // Center the icon on the coordinates
+                })
+            }).addTo(map);
+
             return [lat, lon, intensity]
-          })
+        }).filter(station => station !== null) // Filter out stations with a windSpeed of NaN
   
         L.heatLayer(points, {
             radius: 50,
