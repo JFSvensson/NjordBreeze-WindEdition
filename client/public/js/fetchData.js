dcf3600c394c2e8fc4775067d64c6bfc12ea5e85
Fredrik Svensson <fs222id@student.lnu.se>
2024-05-06 22:22:32 +0200

    Add loading bar

--- client/public/js/fetchData.js
+++ client/public/js/fetchData.js
@@ -1,7 +1,12 @@
 export async function fetchData(url) {
   try {
+    document.getElementById('loading-bar').style.display = 'flex'
+
     const response = await fetch(url)
     const data = await response.json()
+
+    document.getElementById('loading-bar').style.display = 'none'
+
     return data
   } catch (error) {
     console.error('Error fetching data:', error)
