b15e6f212d8c78b0bb0e47bbaba9986dfd923876
Fredrik Svensson <fs222id@student.lnu.se>
2024-05-06 16:48:52 +0200

    Fix button behaviour

--- client/public/js/app.js
+++ client/public/js/app.js
@@ -1,7 +1,7 @@
 import { initializeMap } from './mapSetup.js'
 import { fetchData } from './fetchData.js'
 import { updateMarkers } from './updateMarkers.js'
-import { startUpdates, pauseUpdates, resumeUpdates } from './controls.js'
+import { startUpdates, pauseUpdates, resetUpdates } from './controls.js'
 
 document.addEventListener('DOMContentLoaded', async function() {
     const map = initializeMap()
@@ -25,11 +25,9 @@ document.addEventListener('DOMContentLoaded', async function() {
     }
 
     document.getElementById('start-button').addEventListener('click', () => startUpdates(performUpdates, 500))
-    document.getElementById('pause-button').addEventListener('click', pauseUpdates())
-    document.getElementById('resume-button').addEventListener('click', () => resumeUpdates(performUpdates, 500))
-    document.getElementById('restart-button').addEventListener('click', () => {
-        pauseUpdates()
+    document.getElementById('pause-button').addEventListener('click', () => pauseUpdates())
+    document.getElementById('reset-button').addEventListener('click', () => { 
         hoursPassed = 0 // Reset the hoursPassed
-        startUpdates(performUpdates, 500)
+        resetUpdates()
     })
 })
--- client/public/js/controls.js
+++ client/public/js/controls.js
@@ -1,13 +1,36 @@
+const startButton = document.getElementById('start-button')
+const pauseButton = document.getElementById('pause-button')
+const resetButton = document.getElementById('reset-button')
+const datetimeDisplay = document.getElementById('datetime-display')
+
 let intervalId
 
 export function startUpdates(updateFunction, interval) {
+    if (intervalId) {
+        clearInterval(intervalId)
+    }
     intervalId = setInterval(updateFunction, interval)
+    startButton.textContent = 'Running...'
+    startButton.disabled = true
+    pauseButton.disabled = false
+    pauseButton.textContent = 'Pause'
+    resetButton.disabled = false
 }
 
 export function pauseUpdates() {
     clearInterval(intervalId)
+    pauseButton.disabled = true
+    pauseButton.textContent = 'Paused'
+    startButton.disabled = false
+    startButton.textContent = 'Resume'
 }
 
-export function resumeUpdates(updateFunction, interval) {
-    startUpdates(updateFunction, interval)
+export function resetUpdates() {
+    pauseUpdates()
+    startButton.disabled = false
+    startButton.textContent = 'Start'
+    pauseButton.disabled = true
+    pauseButton.textContent = 'Paused'
+    resetButton.disabled = true
+    datetimeDisplay.innerText = `Data displayed for: ${new Date(1703725200000).toLocaleString()}`
 }
