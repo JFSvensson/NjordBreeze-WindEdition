dbe01f1532b5cfc77421e24da00725fea08f4f5d
Fredrik Svensson <fs222id@student.lnu.se>
2024-05-07 08:30:21 +0200

    Implement slider for changing time

--- client/public/js/app.js
+++ client/public/js/app.js
@@ -5,14 +5,22 @@ import { updateMarkers } from './updateMarkers.js'
 import { startUpdates, pauseUpdates, resetUpdates } from './controls.js'
 
 document.addEventListener('DOMContentLoaded', async function() {
+
+    let hoursPassed = 0
+    let stations
+
     document.getElementById('info-modal').style.display = 'flex'
     document.getElementById('close-info-button').addEventListener('click', () => closeInfo())
+
+    document.getElementById('time-slider').addEventListener('input', (event) => {
+        hoursPassed = parseInt(event.target.value, 10)
+        console.log('Slider set: ', hoursPassed)
+        performUpdates()
+    })
 
     const map = initializeMap()
     displayColorScale()
     const datetimeDisplay = document.getElementById('datetime-display')
-    let hoursPassed = 0
-    let stations
 
     async function performUpdates() {
         let timeToBegin = 1703725200000; // Start date
